9e575837591970eb0a3c20bf5776db20da05ac8d
Matthew Nascimento <matthewromie@hotmail.com>
2023-06-07 17:13:25 -0300

    blockUp ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import styles from './App.module.scss';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      style={{backgroundColor: "hsl(204, 43%, 93%)"}}
+      className={styles.container}
+    >
+      <div className={styles.blockContainer}>
+        <div className={styles.blockUp}>
+          <h1>Join our community</h1>
+          <h2>30-day, hassle-free money back guarantee</h2>
+          <p>Gain acess to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
+        </div>
+        <div className={styles.blockDown}>
+          <div className={styles.blockDownLeft}>
+            <h3>Monthly Subscription</h3>
+            <p><span>$29</span> per month</p>
+            <p>Full access for less than $1 a day</p>
+            <button>Sign Up</button>
+          </div>
+          <div className={styles.blockDownRight}>
+            <h3>Why Us</h3>
+            <p>Tutorials by industry experts</p>
+            <p>Peer & expert code review</p>
+            <p>Coding exercises</p>
+            <p>Access to our GitHub repos</p>
+            <p>Community forum</p>
+            <p>Flashcard decks</p>
+            <p>New videos every week</p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
